6e3068d7e45591d365bfd253f177ac3c0822cd83
Kristen Campbell <kristenc1031@gmail.com>
2021-01-27 19:56:54 -0700

    updated .html

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+var myQuestions = [
+    prompt: "Which of the following tags is used to insert a blank line?," 
+    a: "<p>",
+    b: "<h1>",
+    c: "<hr>",
+    d: "<head>",
+
+]
+
+function start-button() {
+    var Q1 = document.getElementById("question").vaule;
+
+    if (Q1 == "c: <hr>") {
+        confirm("Correct!");
+    } else (Q1 = a: "<p>");
+    return ("Wrong!");
+}
+
+
+var score = 0
+
+// for (var i = 0; i < myQuestions.length; i++) {
+//     var response = prompt(myQuestions[i].prompt);
+//     if (response == questions[i].answers) {
+//         score++;
+//         alert("Correct!");
+//     } else {
+//         alert("WRONG!!");
+//     }
+// }
+
+
+    
+
+
+
+
+
